881b0e40219ad80d0ddea4c94079f632054887e3
Julio Lopez <julioj.lopeza@gmail.com>
2021-04-20 12:03:20 +0200

    Created moviecard component to show movie list

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,7 +3,6 @@ import React, { Component } from 'react'
 class MovieCard extends Component {
   render() {
     const { movie, onMovieClick } = this.props
-    console.log(this.props)
     return <div className="movie-card" onClick={() => onMovieClick(movie)}>{movie.Title}</div>;
   }
 };
