d1a301b26c1d54314be804fc13d9602e4be64c86
Julio Lopez <julioj.lopeza@gmail.com>
2021-04-20 12:01:52 +0200

    Created the views of the app

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Director extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Director</h1>
+      </div>
+    )
+  }
+}
---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Genre extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Genre</h1>
+      </div>
+    )
+  }
+}
