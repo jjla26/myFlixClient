54c2763a7071180f2fa96cbb7c62d426dc49da58
Julio Lopez <julioj.lopeza@gmail.com>
2021-04-26 16:13:07 +0200

    Refactored main view to functional component with hooks

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,89 +1,82 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Spinner } from 'react-bootstrap'
 
+import Navbar from '../navigation/navigation';
 import LoginView from '../login-view/login-view';
 import MovieCard from '../movie-card/movie-card';
 import MovieView from '../movie-view/movie-view';
 import RegistrationView from '../registration-view/registration-view';
+import useRequest from '../../hooks/useRequest'
+import './main-view.scss'
 
-class MainView extends Component {
-  constructor(){
-    super();
-    this.state = {
-      movies: [],
-      selectedMovie: null,
-      user: null,
-      register: false
-    }
-    this.setSelectedMovie = this.setSelectedMovie.bind(this);
-  }
-
-  componentDidMount(){
-    axios.get('https://moviesapi-node.herokuapp.com/movies')
-      .then(response => {
-        this.setState({ movies: response.data.data})
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-
-  /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
-  setSelectedMovie(selectedMovie){
-    this.setState({ selectedMovie })
-  }
-
-  setRegister(register){
-    this.setState({ register })
-  }
+function MainView(){
+  const apiRequest = useRequest()
+  const [ loading, setLoading ] = useState(false)
+  const [ error, setError ] = useState(false)
+  const [ movies, setMovies ] = useState([])
+  const [ selectedMovie, setSelectedMovie ] = useState(null)
+  const [ user, setUser ] = useState(null)
+  const [ register, setRegister ] = useState(false)
 
   /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
-  onLoggedIn(user) {
-    axios.post('https://moviesapi-node.herokuapp.com/login', user)
-    .then(response => {
-      this.setState({ user: response.data.data})
-    })
-    .catch(error => {
-      console.log(error)
-    })
+  const onLoggedIn = async user => {
+    setLoading(true)
+    try {
+      const response = await apiRequest('POST', '/login', user) 
+      setLoading(false)
+      setUser(response)
+    } catch (error) {
+      setLoading(false)
+    }
   }
 
-  onSignUp(user) {
-    axios.post('https://moviesapi-node.herokuapp.com/users', user)
-    .then(response => {
-      this.setState({ register: false })
-    })
-    .catch(error => {
-      console.log(error)
-    })
+  const onSignUp = async user => {
+    setLoading(true)
+    try {
+      await apiRequest('POST', '/users', user)
+      setLoading(false)
+      setRegister(false)
+    } catch (error) {
+      setLoading(false)
+    }
   }
 
-  render() {
-    const { user, movies, selectedMovie, register } = this.state;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await apiRequest('GET', '/movies')
+        setMovies(response)
+      } catch (error) {
+      }
+    }
+    fetchData()
+  },[])
 
-    if(register) return <RegistrationView setRegister={register => this.setRegister(register)} onSignUp={user => this.onSignUp(user)} />
-    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
-    if (!user) return <LoginView setRegister={register => this.setRegister(register)} onLoggedIn={user => this.onLoggedIn(user)} />;
+  if(register) return <RegistrationView loading={loading} setRegister={register => setRegister(register)} onSignUp={user => onSignUp(user)} />
+  /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+  if (!user) return <LoginView loading={loading} setRegister={register => setRegister(register)} onLoggedIn={user => onLoggedIn(user)} />;
 
-    if (movies.length === 0) return <div className="main-view"></div>; // Rendering string if movie array is empty
-  
-    return (
+  if (movies.length === 0) return <div className="main-view"></div>; // Rendering string if movie array is empty
+
+  return (
+    <>
+      <Navbar user={user}/>
       <Row className="main-view d-flex justify-content-md-center">
           {selectedMovie ?
             <Col>
-              <MovieView movie={selectedMovie} onBackButton={this.setSelectedMovie} />
+              <MovieView movie={selectedMovie} onBackButton={movie => setSelectedMovie(movie)} />
             </Col>
           : 
             movies.map(movie => 
             <Col className="d-flex flex-column justify-content-center align-items-center" md={3} key={movie._id} >
-              <MovieCard movie={movie} onMovieClick={this.setSelectedMovie} />
+              <MovieCard movie={movie} onMovieClick={movie => setSelectedMovie(movie)} />
             </Col>
             )
           }
       </Row>
-    );
-  }
+    </>
+  );
 };
 
 export default MainView;
