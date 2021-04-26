7007fd8a6706282a5a1ab35488a317ebc0b08864
Julio Lopez <julioj.lopeza@gmail.com>
2021-04-26 17:01:48 +0200

    Refactored component view with correct use of bootstrap

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import { Container, Image, Row, Button } from 'react-bootstrap'
+import { Row, Col, Image, Button } from 'react-bootstrap'
 import { usePalette } from 'react-palette'
 
 import './movie-view.scss'
@@ -16,30 +16,41 @@ function MovieView(props){
   const { data, loading, error } = usePalette(movie.ImagePath)
 
   return (
-    <Container fluid className="movie-view" style={styles.movieView(data.vibrant)}>
-      <Row className="justify-content-center">
-        <Image className="movie-view__image" src={movie.ImagePath} />
-      </Row>
-      <Row className="justify-content-center">
-        <span className="label">Title: </span>
-        <span className="value">{movie.Title}</span>
-      </Row>
-      <Row className="justify-content-center">
-        <span className="label">Description: </span>
-        <span className="value">{movie.Description}</span>
-      </Row>
-      <Row className="justify-content-center">
-        <span className="label">Genre: </span>
-        <span className="value">{movie.Genre?.Name}</span>
-      </Row>
-      <Row className="justify-content-center">
-        <span className="label">Director: </span>
-        <span className="value">{movie.Director?.Name}</span>
-      </Row>
-      <Row className="justify-content-center">
-        <Button onClick={() => onBackButton(null)}>Back</Button>
-      </Row>
-      </Container>
+    <Row className="movie-view" style={styles.movieView(data.vibrant)}>
+      <Col md={4} className="d-flex justify-content-center align-items-start">
+         <Image className="movie-view__image" src={movie.ImagePath} />
+      </Col>
+      <Col md={8} className="d-flex flex-column justify-content-start">
+        <Row>
+          <Col>
+            Title: {movie.Title}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <span className="label">Description: </span>
+            <span className="value">{movie.Description}</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <span className="label">Genre: </span>
+            <span className="value">{movie.Genre?.Name}</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <span className="label">Director: </span>
+            <span className="value">{movie.Director?.Name}</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button onClick={() => onBackButton(null)}>Back</Button>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   )
 }
 
