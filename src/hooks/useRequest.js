1edcc068c798b43158feddc98c9886ca5bebcf5c
Julio Lopez <julioj.lopeza@gmail.com>
2021-04-27 13:09:01 +0200

    Added header option to useRequest hook

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -4,10 +4,10 @@ import apiUrl from '../utils/api'
 
 function useRequest() { 
 
-  const apiRequest = async (method, path, data = null) => {
+  const apiRequest = async (method, path, data = null, headers = null) => {
     try {
-      const response = await axios({method, url: `${apiUrl}${path}`, data})
-      return response.data.data
+      const response = await axios({method, url: `${apiUrl}${path}`, data, headers})
+      return response.data
     } catch (error) {
       throw error.response.data.message ? error.response.data.message : "Ops! Something happened"
     }
