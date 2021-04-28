64c73ce743f408d8b471ea59fb78386a76b63ef0
Julio Lopez <julioj.lopeza@gmail.com>
2021-04-28 23:25:03 +0200

    Fixed token from the localstorage for all the requests

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -5,13 +5,14 @@ import apiUrl from '../utils/api'
 function useRequest() { 
 
   const apiRequest = async (method, path, data = null, token) => {
+    const accessToken = token ? token : localStorage.getItem('token')
     try {
       const response = await axios({
         method, 
         url: `${apiUrl}${path}`, 
         data, 
         headers: {
-          Authorization: `Bearer ${token}`
+          Authorization: `Bearer ${accessToken}`
         }})
       return response.data
     } catch (error) {
