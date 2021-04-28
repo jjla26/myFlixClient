12bfb54cdee67e58bbe4bfcf93d3064c27f7d5d3
Julio Lopez <julioj.lopeza@gmail.com>
2021-04-28 22:54:43 +0200

    Added profie view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,11 +1,34 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
+import { Card, Row, Col, ListGroup, Button } from 'react-bootstrap'
 
-export default class Profile extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Profile</h1>
-      </div>
-    )
-  }
+import userlogo from 'url:../../img/user.svg' 
+import './profile-view.scss'
+
+function Profile(props){
+const { user } = props
+console.log(user)
+
+  return (
+    <Row className="profile-view d-flex justify-content-center align-items-center">
+      <Col md={4}>
+        <Card className="profile-card text-center">
+          <Card.Img variant="top" className="profile-card__image" src={userlogo} />
+          <Card.Body>
+            <Button>Update</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col md={8}>
+      <Card className="profile-card">
+          <ListGroup variant="flush">
+            <ListGroup.Item  className="profile-details">Username: {user}</ListGroup.Item>
+            <ListGroup.Item  className="profile-details">Email: </ListGroup.Item>
+            <ListGroup.Item  className="profile-details">Birdthday: </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  )
 }
+
+export default Profile
