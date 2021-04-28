699679710713b45356bbdad6aba302eb38898cc4
Julio Lopez <julioj.lopeza@gmail.com>
2021-04-28 12:15:19 +0200

    Added auth to useREquest hook

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -4,9 +4,15 @@ import apiUrl from '../utils/api'
 
 function useRequest() { 
 
-  const apiRequest = async (method, path, data = null, headers = null) => {
+  const apiRequest = async (method, path, data = null, token) => {
     try {
-      const response = await axios({method, url: `${apiUrl}${path}`, data, headers})
+      const response = await axios({
+        method, 
+        url: `${apiUrl}${path}`, 
+        data, 
+        headers: {
+          Authorization: `Bearer ${token}`
+        }})
       return response.data
     } catch (error) {
       throw error.response.data.message ? error.response.data.message : "Ops! Something happened"
