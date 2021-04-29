12fec8039e6427e5ac326430494dd80fab31803e
Julio Lopez <julioj.lopeza@gmail.com>
2021-04-29 13:50:49 +0200

    Added profile update feature to profile view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,17 +1,23 @@
-import React, { useState } from 'react'
-import { Card, Row, Col, ListGroup, Button, Spinner, Alert } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Card, Row, Col, ListGroup, Button, Spinner, Alert, Form, InputGroup } from 'react-bootstrap'
+import { PersonFill, KeyFill, Calendar2DateFill } from 'react-bootstrap-icons'
+import { Formik } from 'formik'
 
 import useRequest from '../../hooks/useRequest' 
+import Validation from '../../utils/registerValidation' 
 import userlogo from 'url:../../img/user.svg' 
 import Modal from '../modal/modal' 
 import './profile-view.scss'
+import moment from 'moment'
 
 function Profile(props){
   const apiRequest = useRequest()
-  const { userDetails, onLoggedOut } = props
+  const { userDetails, onLoggedOut, setUserDetails, setUser } = props
   const [ error, setError ] = useState(null)
+  const [ message, setMessage ] = useState(null)
   const [ loading, setLoading ] = useState(false)
   const [ showModal, setShowModal ] = useState(false) 
+  const [ update, setUpdate ] = useState(false)
 
   onDeleteAcc = async () => {
     setLoading(true)
@@ -20,12 +26,52 @@ function Profile(props){
       setLoading(false)
       onLoggedOut()
     } catch (error) {
-      console.log(error)
       setLoading(false)
-      setShowModal(true)
+      setShowModal(false)
       setError(error)
     }
   }
+
+  handleSubmit = async (values) => {
+    setLoading(true)
+    try {
+      const response = await apiRequest('PUT', `/users/${userDetails.Username}`, {
+        ...(values.username ? { Username: values.username } : {}),
+        ...(values.email ? { Email: values.email } : {}),
+        ...(values.password ? { Password: values.password } : {}),
+        ...(values.birthday ? { Birthday : values.birthday } : {}),
+      })
+      localStorage.setItem('user', response.data.Username);
+      setUser(response.data.Username)
+      setUserDetails(response.data)
+      setLoading(false)
+      setUpdate(false)
+      setMessage(response.message)
+    } catch (error) {
+      setLoading(false)
+      setError(error)
+    }
+  }
+
+  useEffect(() => {
+    let timeout
+    if(error){
+      timeout = setTimeout(() => setError(null), 3000)
+    }
+    return () => {
+      clearTimeout(timeout)
+    }
+  },[error])
+
+  useEffect(() => {
+    let timeout
+    if(message){
+      timeout = setTimeout(() => setMessage(null), 3000)
+    }
+    return () => {
+      clearTimeout(timeout)
+    }
+  },[message])
 
   return (
     <Row className="profile-view d-flex justify-content-center align-items-center">
@@ -36,19 +82,147 @@ function Profile(props){
             <Spinner animation="grow" variant="primary"/>
           </Card.Body>
           :
+          update ? 
           <Card.Body>
-            <Button className="m-2">Update</Button>
+            <Button className="m-2" onClick={() => setUpdate(false)}>Cancel Update</Button>
+          </Card.Body>
+          :
+          <Card.Body>
+            <Button className="m-2" onClick={() => setUpdate(true)}>Update</Button>
             <Button variant="danger" className="m-2" onClick={() => setShowModal(true)}>Delete my account</Button>
           </Card.Body>}
         </Card>
       </Col>
       <Col md={8}>
-      <Card className="profile-card">
-          <ListGroup variant="flush">
-            <ListGroup.Item  className="profile-details">Username: {userDetails.Username}</ListGroup.Item>
-            <ListGroup.Item  className="profile-details">Email: {userDetails.Email}</ListGroup.Item>
-            <ListGroup.Item  className="profile-details">Birdthday: {userDetails.Birthday}</ListGroup.Item>
-          </ListGroup>
+        <Card className="profile-card">
+          {update ?
+            <Row className="d-flex justify-content-center align-items-center">
+              <Col md={8}>
+                <Formik
+                  validate={Validation}
+                  onSubmit={handleSubmit}
+                  initialValues={{
+                    username: userDetails.Username,
+                    email: userDetails.Email,
+                    password: '',
+                    passwordConfirmation: '',
+                    birthday: moment(userDetails.Birthday).format('YYYY-MM-DD'),
+                  }}
+                >
+                  {({
+                    handleSubmit,
+                    handleChange,
+                    handleBlur,
+                    touched,
+                    values,
+                    errors,
+                  }) => (
+                    <Form>
+                      <Form.Group controlId="formUsername">
+                        <InputGroup>
+                          <InputGroup.Prepend className="update-card__prepend">
+                            <PersonFill />
+                          </InputGroup.Prepend>
+                          <Form.Control 
+                            isInvalid={touched.username && !!errors.username} 
+                            name="username" type="text" placeholder="username" 
+                            value={values.username} 
+                            onChange={handleChange} 
+                            onBlur={handleBlur}  />
+                          <Form.Control.Feedback type="invalid">
+                              {errors.username}
+                          </Form.Control.Feedback>
+                        </InputGroup>
+                      </Form.Group>
+
+                      <Form.Group controlId="formEmail">
+                        <InputGroup>
+                          <InputGroup.Prepend className="update-card__prepend">
+                            @
+                          </InputGroup.Prepend>
+                          <Form.Control 
+                            isInvalid={touched.email && !!errors.email} 
+                            name="email" type="email" placeholder="email" 
+                            value={values.email} 
+                            onChange={handleChange} 
+                            onBlur={handleBlur}  />
+                          <Form.Control.Feedback type="invalid">
+                            {errors.email}
+                          </Form.Control.Feedback>
+                        </InputGroup>
+                      </Form.Group>
+
+                      <Form.Group controlId="formPassword">
+                        <InputGroup className="mb-2">
+                          <InputGroup.Prepend className="update-card__prepend">
+                            <KeyFill />
+                          </InputGroup.Prepend>
+                          <Form.Control 
+                            isInvalid={touched.password && !!errors.password} 
+                            name="password" autoComplete="new-password" 
+                            type="password" placeholder="password" 
+                            value={values.password} 
+                            onChange={handleChange} 
+                            onBlur={handleBlur} />
+                          <Form.Control.Feedback type="invalid">
+                            {errors.password}
+                          </Form.Control.Feedback>
+                        </InputGroup>
+                      </Form.Group>
+
+                      <Form.Group controlId="formPasswordConfirmation">
+                        <InputGroup className="mb-2">
+                          <InputGroup.Prepend className="update-card__prepend">
+                            <KeyFill />
+                          </InputGroup.Prepend>
+                          <Form.Control 
+                            isInvalid={touched.passwordConfirmation && !!errors.passwordConfirmation} 
+                            name="passwordConfirmation" autoComplete="current-password" 
+                            type="password" placeholder="Password Confirmation" 
+                            value={values.passwordConfirmation} 
+                            onChange={handleChange} 
+                            onBlur={handleBlur} />
+                          <Form.Control.Feedback type="invalid">
+                            {errors.passwordConfirmation}
+                          </Form.Control.Feedback>
+                        </InputGroup>
+                      </Form.Group>
+
+                      <Form.Group controlId="formBirthday">
+                        <InputGroup>
+                          <InputGroup.Prepend className="update-card__prepend">
+                            <Calendar2DateFill />
+                          </InputGroup.Prepend>
+                          <Form.Control 
+                            isInvalid={touched.birthday && !!errors.birthday} 
+                            name="birthday" type="date" placeholder="birthday" 
+                            value={values.birthday} 
+                            onChange={handleChange} 
+                            onBlur={handleBlur} />
+                          <Form.Control.Feedback type="invalid">
+                            {errors.birthday}
+                          </Form.Control.Feedback>
+                        </InputGroup>
+                      </Form.Group>
+
+                      {loading ? 
+                      <Spinner className="float-right" animation="grow" variant="primary"/>
+                      :
+                      <Button className="float-right" variant="primary" type="submit" onClick={handleSubmit}>
+                        Update
+                      </Button>}
+                    </Form>
+                  )}
+              </Formik>
+            </Col>
+          </Row>
+          : 
+            <ListGroup variant="flush">
+              <ListGroup.Item  className="profile-details">Username: {userDetails.Username}</ListGroup.Item>
+              <ListGroup.Item  className="profile-details">Email: {userDetails.Email}</ListGroup.Item>
+              <ListGroup.Item  className="profile-details">Birdthday: {moment(userDetails.Birthday).format('DD.MM.YYYY')}</ListGroup.Item>
+            </ListGroup>
+          }
         </Card>
       </Col>
       <Modal 
@@ -60,6 +234,7 @@ function Profile(props){
         show={showModal}
       />
       <Alert show={!!error} className="error-message" variant="secondary">{error}</Alert>
+      <Alert show={!!message} className="success-message" variant="secondary">{message}</Alert>
     </Row>
   )
 }
