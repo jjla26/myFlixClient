8d93c75722de3cdbec2465889f2065f2753567cc
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-04 23:02:44 +0200

    Added reducers and actions for the rest of the state

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,10 +1,30 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
+export const SET_USER = 'SET_USER';
+export const SET_USER_DETAILS = 'SET_USER_DETAILS';
+export const SET_MESSAGE = 'SET_MESSAGE';
+export const SET_ERROR = 'SET_ERROR'
 
-export function setMovies(value) {
-  return { type: SET_MOVIES, value };
+export function setUser(payload){
+  return { type: SET_USER, payload }
 }
 
-export function setFilter(value) {
-  return { type: SET_FILTER, value };
+export function setUserDetails(payload){
+  return { type: SET_USER_DETAILS, payload }
+}
+
+export function setMessage(payload){
+  return { type: SET_MESSAGE, payload }
+}
+
+export function setError(payload){
+  return { type: SET_ERROR, payload }
+}
+
+export function setMovies(payload) {
+  return { type: SET_MOVIES, payload };
+}
+
+export function setFilter(payload) {
+  return { type: SET_FILTER, payload };
 }
--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -1,11 +1,47 @@
 import { combineReducers } from 'redux';
 
-import { SET_FILTER, SET_MOVIES } from '../actions/actions';
+import { SET_FILTER, SET_MOVIES, SET_USER, SET_MESSAGE, SET_ERROR, SET_USER_DETAILS } from '../actions/actions';
+
+function user(state = '', action) {
+  switch (action.type) {
+    case SET_USER:
+      return action.payload
+    default:
+      return state;
+  }
+}
+
+function message(state = '', action) {
+  switch (action.type) {
+    case SET_MESSAGE:
+      return action.payload
+    default:
+      return state;
+  }
+}
+
+function error(state = '', action) {
+  switch (action.type) {
+    case SET_ERROR:
+      return action.payload
+    default:
+      return state;
+  }
+}
+
+function userDetails(state = {}, action) {
+  switch (action.type) {
+    case SET_USER_DETAILS:
+      return action.payload
+    default:
+      return state;
+  }
+}
 
 function visibilityFilter(state = '', action) {
   switch (action.type) {
     case SET_FILTER:
-      return action.value;
+      return action.payload;
     default:
       return state;
   }
@@ -14,7 +50,7 @@ function visibilityFilter(state = '', action) {
 function movies(state = [], action) {
   switch (action.type) {
     case SET_MOVIES:
-      return action.value;
+      return action.payload;
     default:
       return state;
   }
@@ -22,7 +58,11 @@ function movies(state = [], action) {
 
 const moviesApp = combineReducers({
   visibilityFilter,
-  movies
+  movies,
+  user,
+  userDetails,
+  message,
+  error
 });
 
 export default moviesApp;
