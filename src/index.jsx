779cbc5aae3a8a4258e16bed9211019875e26066
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-04 10:45:34 +0200

    Added react-redux and redux configuration

--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,25 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import { Container } from 'react-bootstrap'
 import MainView from './components/main-view/main-view';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension'
 
+import moviesApp from './redux/reducers/reducers';
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
+
+const store = createStore(moviesApp, devToolsEnhancer());
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends Component {
   render() {
     return (
-      <Container className="my-flix" fluid>
-        <MainView/>
-      </Container>
+      <Provider store={store}>
+        <Container className="my-flix" fluid>
+          <MainView/>
+        </Container>
+      </Provider>
     );
   }
 }
