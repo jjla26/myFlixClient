b6e7fd279c9cbf994435cf0f5f16cb55195fc10a
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-05 14:20:19 +0200

    Refactored bootstrap structure of the project

--- src/components/favorites-view/favorites-view.jsx
+++ src/components/favorites-view/favorites-view.jsx
@@ -10,23 +10,25 @@ function FavoritesView() {
   const userDetails = useSelector(state => state.userDetails)
   
   return (
-    <Col>
-      <Row className="d-flex justify-content-center">
-        <h4>My List of Favorites Movies</h4>
-      </Row>
-      <Row className="favorites-movies d-flex justify-content-center">
-        {userDetails.FavoriteMovies && userDetails.FavoriteMovies.length > 0 ? 
-        movies.filter(movie => userDetails.FavoriteMovies && userDetails.FavoriteMovies.find(fav => fav === movie._id)).map( 
-          movie => <Col className="d-flex justify-content-center align-items-center" md={3} key={movie._id}>
-            <MovieCard movie={movie} />
-          </Col>)
-        : 
-        <Col className="d-flex justify-content-center p-5">
-          <h6>Add movies to your favorites</h6>
-        </Col>
-        }
-      </Row>
-    </Col>
+    <Row>
+      <Col>
+        <Row className="d-flex justify-content-center">
+          <h4>My List of Favorites Movies</h4>
+        </Row>
+        <Row className="favorites-movies d-flex justify-content-center">
+          {userDetails.FavoriteMovies && userDetails.FavoriteMovies.length > 0 ? 
+          movies.filter(movie => userDetails.FavoriteMovies && userDetails.FavoriteMovies.find(fav => fav === movie._id)).map( 
+            movie => <Col className="d-flex justify-content-center align-items-center" md={3} key={movie._id}>
+              <MovieCard movie={movie} />
+            </Col>)
+          : 
+          <Col className="d-flex justify-content-center p-5">
+            <h6>Add movies to your favorites</h6>
+          </Col>
+          }
+        </Row>
+      </Col>
+    </Row>
   )
 }
 
--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Row, Col, Button } from 'react-bootstrap'
 
-import MovieList from '../movie-list/movie-list'
+import MovieCard from '../movie-card/movie-card'
 import './genre-view.scss'
 
 function GenreView(props){
@@ -11,33 +11,40 @@ function GenreView(props){
   const movie = movies.find(movie => movie.Genre.Name === match.params.name)
 
   return (
-    <Col>
-      <Row className="genre-view d-flex flex-column justify-content-center align-items-center">
-        <Col md={10} className="d-flex flex-column justify-content-center align-items-center">
-          <Row p={5}>
-            <Col className="m-2">
-              Name: {movie?.Genre.Name}
+    <Row>
+
+      <Col>
+        <Row className="genre-view d-flex flex-column justify-content-center align-items-center">
+          <Col md={10} className="d-flex flex-column justify-content-center align-items-center">
+            <Row p={5}>
+              <Col className="m-2">
+                Name: {movie?.Genre.Name}
+              </Col>
+            </Row>
+            <Row>
+              <Col className="m-2">
+                Description: {movie?.Genre.Description}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Button onClick={() => history.goBack()}>Back</Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row className="d-flex justify-content-center">
+          <h4>{match.params.name} Movies</h4>
+        </Row>
+        <Row className="genre-movies d-flex justify-content-center">
+          {movies.filter(movie => movie.Genre.Name === match.params.name).map(movie => 
+            <Col className="d-flex justify-content-center align-items-center" md={3} key={movie._id} >
+              <MovieCard movie={movie} />
             </Col>
-          </Row>
-          <Row>
-            <Col className="m-2">
-              Description: {movie?.Genre.Description}
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Button onClick={() => history.goBack()}>Back</Button>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row className="d-flex justify-content-center">
-        <h4>{match.params.name} Movies</h4>
-      </Row>
-      <Row className="d-flex justify-content-center">
-        <MovieList movies={movies.filter(movie => movie.Genre.Name === match.params.name)} />
-      </Row>
-    </Col>
+          )}
+        </Row>
+      </Col>
+    </Row>
   )
 }
 
