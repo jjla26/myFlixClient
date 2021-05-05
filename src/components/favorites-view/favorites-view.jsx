5a6baf3a2729d4409dafd0df61b1af54cdee8892
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-05 13:39:09 +0200

    Added margin to favorites view

--- src/components/favorites-view/favorites-view.jsx
+++ src/components/favorites-view/favorites-view.jsx
@@ -2,20 +2,24 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Col, Row } from 'react-bootstrap'
 
-import MovieList from '../movie-list/movie-list'
+import MovieCard from '../movie-card/movie-card'
+import './favorites-view.scss'
 
 function FavoritesView() {
   const movies = useSelector(state => state.movies)
   const userDetails = useSelector(state => state.userDetails)
   
   return (
-    <Col>
+    <Col className="favorites-view">
       <Row className="d-flex justify-content-center">
         <h4>My List of Favorites Movies</h4>
       </Row>
-      <Row>
+      <Row className="m-5">
         {userDetails.FavoriteMovies && userDetails.FavoriteMovies.length > 0 ? 
-        <MovieList movies={movies.filter(movie => userDetails.FavoriteMovies && userDetails.FavoriteMovies.find(fav => fav === movie._id))} />
+        movies.filter(movie => userDetails.FavoriteMovies && userDetails.FavoriteMovies.find(fav => fav === movie._id)).map( 
+          movie => <Col className="d-flex justify-content-center align-items-center" md={3} key={movie._id}>
+            <MovieCard movie={movie} />
+          </Col>)
         : 
         <Col className="d-flex justify-content-center p-5">
           <h6>Add movies to your favorites</h6>
