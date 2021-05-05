811824f8fa8f533fb852c41d28a3026eddc4d6ec
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-05 13:03:32 +0200

    Refactored main-view to use redux

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
-import { Row, Col, Alert } from 'react-bootstrap'
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { Row, Alert } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux'
 
 import { setMovies, setUser, setUserDetails, setMessage, setError } from '../../redux/actions/actions'
@@ -19,13 +19,9 @@ import './main-view.scss'
 function MainView(){
   const dispatch = useDispatch()
   const apiRequest = useRequest()
-  const movies = useSelector(state => state.movies)
   const message = useSelector(state => state.message)
   const error = useSelector(state => state.error)
   const user = useSelector(state => state.user)
-  const userDetails = useSelector(state => state.userDetails)
-
-
 
   const getMovies = async () => {
     try {
@@ -40,32 +36,6 @@ function MainView(){
     try {
       const response = await apiRequest('GET', `/users/${name}`)
       dispatch(setUserDetails(response.data))
-    } catch (error) {
-      dispatch(setError(error))
-    }
-  }
-
-  const onLoggedOut = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    dispatch(setUser(null));
-    dispatch(setMessage("You have logged out successfully  "))
-  }
-
-  const onAddFavorite = async movie => {
-    try {
-      const response = await apiRequest('POST', `/users/${user}/favorites/${movie}`)
-      dispatch(setUserDetails(response.data));
-      dispatch(setMessage(response.message))
-    } catch (error) {
-      setError(error)
-    }
-  } 
-  const onDeleteFavorite = async movie => {
-    try {
-      const response = await apiRequest('DELETE', `/users/${user}/favorites/${movie}`)
-      dispatch(setUserDetails(response.data))
-      dispatch(setMessage(response.message))
     } catch (error) {
       dispatch(setError(error))
     }
@@ -105,59 +75,23 @@ function MainView(){
 
   return (
     <Router>
-      {user && <Navbar user={user} onLoggedOut={onLoggedOut}/>}
+      <Row className="main-view">
+        {user && <Navbar />}
+      </Row>
       <Row className="main-view d-flex justify-content-md-center">
-        <Route path="/myfavorites" render={() => {
-          if (movies.length === 0) return <div className="main-view" />;
-          if(!user) return <Redirect to="/" />
-
-          return (
-            <Col>
-              <Row className="d-flex justify-content-center">
-                <h4>My List of Favorites Movies</h4>
-              </Row>
-              <Row>
-                {userDetails.FavoriteMovies && userDetails.FavoriteMovies.length > 0 ? 
-                <MovieList movies={movies.filter(movie => userDetails.FavoriteMovies && userDetails.FavoriteMovies.find(fav => fav === movie._id))} />
-                : 
-                <Col className="d-flex justify-content-center p-5">
-                  <h6>Add movies to your favorites</h6>
-                </Col>
-                }
-              </Row>
-            </Col>
-          )
-        }} />
-        <Route path="/profile" render={() => {
-          if (movies.length === 0) return <div className="main-view" />;
-          if(!user) return <Redirect to="/" />
-          return (
-            <Col>
-              <ProfileView onLoggedOut={onLoggedOut}/>
-            </Col>
-          )
-        }} />
-        <Route path="/movies/:movieId" render={({ match, history }) => {
-          if (movies.length === 0) return <div className="main-view" />;
-          if(!user) return <Redirect to="/" />
-          return (<Col>
-            <MovieView 
-              movie={movies.find(movie => movie._id === match.params.movieId)} 
-              favorite={userDetails.FavoriteMovies && userDetails.FavoriteMovies.find(movie => movie === match.params.movieId )} 
-              onAddFavorite={onAddFavorite}
-              onDeleteFavorite={onDeleteFavorite}
-              onBackButton={() => history.goBack()} />
-          </Col>
-          )
-        }}/>
+        <Route path="/myfavorites" component={MovieList} />          
+        <Route path="/movies/:movieId" component={MovieView} />          
         <Route path="/director/:name" component={DirectorView} />
         <Route path="/genre/:name" component={GenreView} />
+        <Route path="/profile" component={ProfileView} /> 
         <Route path="/register" component={RegistrationView} />
         <Route exact={true} path="/" component={user ? MovieList : LoginView} />
+        <Alert show={!!error} className="error-message" variant="secondary">{error}</Alert>
+        <Alert show={!!message} className="success-message" variant="primary">{message}</Alert>
       </Row>
-      <Footer />
-      <Alert show={!!error} className="error-message" variant="secondary">{error}</Alert>
-      <Alert show={!!message} className="success-message" variant="primary">{message}</Alert>
+      <Row className="main-view">
+        <Footer />
+      </Row>
     </Router>
   );
 };
