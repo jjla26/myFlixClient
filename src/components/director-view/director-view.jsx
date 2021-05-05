d3cd1937724885f8cb2cb07acc008d1682105b90
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-05 16:21:30 +0200

    Fixed white line director view

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -12,42 +12,45 @@ function DirectorView(props){
   const movie = movies.find(movie => movie.Director.Name === match.params.name)
   
   return (
-    <Col>
-      <Row className="director-view d-flex flex-column justify-content-center align-items-center">
-        <Col md={10} className="d-flex flex-column justify-content-center align-items-center">
-          <Row p={5}>
-            <Col className="m-2">
-              Name: {movie?.Director.Name}
-            </Col>
-          </Row>
-          <Row>
-            <Col className="m-2">
-              Birthdate: {moment(movie?.Director.Birth).format('DD-MM-YYYY')}
-            </Col>
-          </Row>
-          <Row>
-            <Col className="m-2">
-              Description: {movie?.Director.Bio}
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Button onClick={() => history.goBack()}>Back</Button>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row className="d-flex justify-content-center">
-        <h4>{match.params.name}'s Movies</h4>
-      </Row>
-      <Row className="director-movies d-flex justify-content-center">
-        {movies.filter(movie => movie.Director.Name === match.params.name).map(movie => 
-          <Col className="d-flex justify-content-center align-items-center" md={3} key={movie._id} >
-            <MovieCard movie={movie} />
+    <Row>
+
+      <Col>
+        <Row className="director-view d-flex flex-column justify-content-center align-items-center">
+          <Col md={10} className="d-flex flex-column justify-content-center align-items-center">
+            <Row p={5}>
+              <Col className="m-2">
+                Name: {movie?.Director.Name}
+              </Col>
+            </Row>
+            <Row>
+              <Col className="m-2">
+                Birthdate: {moment(movie?.Director.Birth).format('DD-MM-YYYY')}
+              </Col>
+            </Row>
+            <Row>
+              <Col className="m-2">
+                Description: {movie?.Director.Bio}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Button onClick={() => history.goBack()}>Back</Button>
+              </Col>
+            </Row>
           </Col>
-        )}
-      </Row>
-    </Col>
+        </Row>
+        <Row className="d-flex justify-content-center">
+          <h4>{match.params.name}'s Movies</h4>
+        </Row>
+        <Row className="director-movies d-flex justify-content-center">
+          {movies.filter(movie => movie.Director.Name === match.params.name).map(movie => 
+            <Col className="d-flex justify-content-center align-items-center" md={3} key={movie._id} >
+              <MovieCard movie={movie} />
+            </Col>
+          )}
+        </Row>
+      </Col>
+    </Row>
   )
 }
 
