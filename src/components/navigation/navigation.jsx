3f1c257a2500213f3985c3f0634c30bc9695d977
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-05 13:06:45 +0200

    Added Col to navigation

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav, Col, NavDropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 import useLogOut from '../../hooks/useLogOut'
@@ -11,24 +11,26 @@ function Navigation() {
   const onLoggedOut = useLogOut()
   const user = useSelector( state => state.user)
   return (
-    <Navbar className="navigation" collapseOnSelect expand="lg" bg="secondary" variant="primary" sticky="top">
-      <Navbar.Brand as={Link} to='/'>MYFLIX APP</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to='/'>Home</Nav.Link>
-          <Nav.Link as={Link} to='/myfavorites'>Favorites</Nav.Link>
-        </Nav>
-        <Nav>
-          <VisibilityFilterInput  />
-          <NavDropdown title={`${user}'s Profile`} id="basic-nav-dropdown">
-          <NavDropdown.Item as={Link} to={`/profile`}>My Profile</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item onClick={onLoggedOut}>Log Out</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <Col>
+      <Navbar className="navigation" collapseOnSelect expand="lg" bg="secondary" variant="primary" sticky="top">
+        <Navbar.Brand as={Link} to='/'>MYFLIX APP</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to='/'>Home</Nav.Link>
+            <Nav.Link as={Link} to='/myfavorites'>Favorites</Nav.Link>
+          </Nav>
+          <Nav>
+            <VisibilityFilterInput  />
+            <NavDropdown title={`${user}'s Profile`} id="basic-nav-dropdown">
+            <NavDropdown.Item as={Link} to={`/profile`}>My Profile</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item onClick={onLoggedOut}>Log Out</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Col>
   )
 }
 
